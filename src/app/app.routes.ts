b83e596d3e42b5b0c05b5b9fdbfd4790816ebfae
Tetiana Zelenska <tetianazelenska@Tetianas-MacBook-Air.local>
2024-02-02 00:16:53 +0100

    basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./features/goods/goods.component').then(mod => mod.GoodsComponent)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/features/goods/goods.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goods',
+  standalone: true,
+  imports: [],
+  templateUrl: './goods.component.html',
+  styleUrl: './goods.component.scss'
+})
+export class GoodsComponent {
+
+}
